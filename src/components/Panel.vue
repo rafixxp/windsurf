17fa31990a0c0d7d8488933f81805611e558312e
rafixxp <amotcamo69@gmail.com>
2025-06-18 20:23:49 +0700

    fix:profile

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -14,10 +14,10 @@
                     <span class="fs-21">Run</span>
                 </router-link>
             </div>
-            
+
             <div class="col text-center p-0">
-                <router-link to="/" class="text-decoration-none text-dark">
-                    <i class="fas fa-user"></i><br>
+                <router-link to="/transaksi" class="text-decoration-none text-dark">
+                    <i class="fas fa-user fs-4"></i><br>
                     <span class="fs-21">Profile</span>
                 </router-link>
             </div>
